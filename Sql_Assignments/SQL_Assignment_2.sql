5de9ce3deef1bf291c39399bf35e8b7bd382aaae
Gajulasridhar <sridharyadav0852@gmail.com>
2024-07-26 17:36:33 +0530

    added_SQL_2

--- Sql_Assignments/SQL_Assignment_2.sql
+++ Sql_Assignments/SQL_Assignment_2.sql
@@ -77,3 +77,59 @@ select emp_no from works_on where job='clerk'
 --Q3)
 
 select emp_no from works_on where project_no='p2' and emp_no <10000;
+
+--Q4)
+select emp_no from works_on where year(enter_date)!=1998
+
+--Q5)
+select emp_no from works_on  where project_no='p1' and job in('Analyst','manager')
+
+--Q6)
+select enter_date from works_on where project_no='p2' and job not in('Analyst','manager','Clerk')
+
+--Q7)
+select emp_no,emp_lname from Employee where emp_fname like '%tt%'
+
+--Q8)
+select emp_no,emp_fname from Employee where emp_lname like '_o%es' or emp_lname like '_a%es'
+
+--Q9)
+
+select e.emp_no from Employee e join Department d on e.Dept_no=d.Dept_no where d.location='Seattle'
+
+--Q10)
+select e.emp_fname,e.emp_lname from Employee e join works_on w on e.emp_no=w.emp_no where w.enter_date='1998-01-04'
+
+--Q11)
+
+SELECT 
+    e.emp_no, 
+    e.emp_fname, 
+    e.emp_lname, 
+    e.dept_no, 
+    d.Dept_name, 
+    d.location 
+FROM 
+    Employee e 
+JOIN 
+    Department d 
+ON 
+    d.Dept_no = e.Dept_no 
+GROUP BY 
+    d.location, 
+    e.emp_no, 
+    e.emp_fname, 
+    e.emp_lname, 
+    e.dept_no, 
+    d.Dept_name;
+--12Q)
+select MAX(emp_no) from works_on
+--13Q)
+
+SELECT Job FROM Works_on GROUP BY Job HAVING COUNT(emp_no) > 2;
+
+--14Q)
+
+SELECT emp_no FROM Works_on WHERE Job = 'Clerk'
+UNION
+SELECT emp_no FROM Employee WHERE dept_no = 'd3';
