00b002c258de6941a7705caf286493f5e00c915c
Gajulasridhar <sridharyadav0852@gmail.com>
2024-07-30 22:01:58 +0530

    added changes

--- Asp.net Assignments/MVC/MVC_Assignment/Models/MovieRepository.cs
+++ Asp.net Assignments/MVC/MVC_Assignment/Models/MovieRepository.cs
@@ -4,9 +4,9 @@
     {
         static List<Movie> movies = new List<Movie>()
         {
-            new Movie(){MovieId=1,Title="Kalki",Actor="prabhas",Language="Telugu",Director="NagAswin" },
-             new Movie(){MovieId=1,Title="Kalki",Actor="prabhas",Language="Telugu",Director="NagAswin"},
-              new Movie(){MovieId=1,Title="Kalki",Actor="prabhas",Language="Telugu",Director="NagAswin"},
+                new Movie(){MovieId=1,Title="Kalki",Actor="Prabhas",Language="Telugu",Director="NagAswin" },
+            new Movie(){MovieId=2,Title="Bahubali",Actor="Prabhas",Language="Telugu",Director="Rajamouli"},
+            new Movie(){MovieId=3,Title="Sye",Actor="Nitin",Language="Telugu",Director="Rajamouli"},
 
         };
         public List<Movie> GetMovies()
@@ -17,7 +17,7 @@
         public Movie GetMovieByName(string title)
         {
 
-            return movies.Single(p => p.Title == title);
+            return movies.SingleOrDefault(m => string.Equals(m.Title, title, StringComparison.OrdinalIgnoreCase));
         }
         public void Add(Movie movie)
         {
